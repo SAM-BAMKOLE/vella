a228fb6d0da5cfb31c936db5ddbee8420a158f20
samuel <olagoroyesamuel7@gmail.com>
2026-09-11 13:51:48 +0100

    Added favicons

--- sw.js
+++ sw.js
@@ -1,19 +1,28 @@
-const CACHE_NAME = 'vella-pwa-v1';
+const CACHE_NAME = "vella-pwa-v1";
 const ASSETS = [
-  './',
-  './index.html',
-  'https://unpkg.com/@tailwindcss/browser@4',
-  'https://cdn.jsdelivr.net/npm/html2canvas-pro@1.5.8/dist/html2canvas-pro.min.js'
+  "./",
+  "./index.html",
+  "./favicon.ico",
+  "./favicon-32x32.png",
+  "./favicon-16x16.png",
+  "./apple-touch-icon.png",
+  "./android-chrome-192x192.png",
+  "./android-chrome-512x512.png",
+  "./site.webmanifest",
+  "https://unpkg.com/@tailwindcss/browser@4",
+  "https://cdn.jsdelivr.net/npm/html2canvas-pro@1.5.8/dist/html2canvas-pro.min.js",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request)),
   );
 });
